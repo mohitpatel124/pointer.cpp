1f5deabb6d294b158001c10df12a0cca333f54cd
mohitpatel124 <106043336+mohitpatel124@users.noreply.github.com>
2023-05-17 18:46:15 -0700

    Add files via upload

---
+++ pointer/pointer1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout<<"enter number of elements";
+    cin>>size;
+    int A[size];
+    cout<<sizeof A<<endl;
+
+    return 0;
+
+}
